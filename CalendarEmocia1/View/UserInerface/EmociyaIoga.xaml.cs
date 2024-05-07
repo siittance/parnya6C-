bcd610b1345ba1ca210776169002bf4e148f2c88
siittance <144900566+siittance@users.noreply.github.com>
2024-05-07 10:08:25 +0300

    Update EmociyaIoga.xaml.cs

--- CalendarEmocia1/View/UserInerface/EmociyaIoga.xaml.cs
+++ CalendarEmocia1/View/UserInerface/EmociyaIoga.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using CalendarEmocia1.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,11 +21,10 @@ namespace CalendarEmocia1.View.UserInerface
     /// </summary>
     public partial class EmociyaIoga : UserControl
     {
-        public string ioging { get; set; }
         public EmociyaIoga()
         {
             InitializeComponent();
-            DataContext = this;
+            DataContext = new VyborNastroeniaViewModel();
         }
     }
 }
